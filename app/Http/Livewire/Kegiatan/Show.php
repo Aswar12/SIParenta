cfe1f70704bdec053b39e2b5585d86b5e14783dc
Aswar12 <aswarthedoctor@gmail.com>
2022-03-21 15:21:47 +0800

    ac

--- app/Http/Livewire/Kegiatan/Show.php
+++ app/Http/Livewire/Kegiatan/Show.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Livewire\Kegiatan;
 
+use Illuminate\View\Component as ViewComponent;
 use Livewire\Component;
+
 
 class Show extends Component
 {
     public function render()
     {
         return view('livewire.kegiatan.show');
+    }
+
+
+    public $openmodal=0;
+    public function AddKegiatan()
+    {
+        $this->openmodal=1;
+       
+    }
+    public function closemodal()
+    {
+        $this->openmodal=0;
     }
 }
