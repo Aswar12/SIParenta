f4959ab77024ab9f5ea937f3af6f88e3d1a8f23a
Aswar12 <aswarthedoctor@gmail.com>
2022-03-23 11:32:00 +0800

    1

--- resources/views/livewire/kegiatan/show.blade.php
+++ resources/views/livewire/kegiatan/show.blade.php
@@ -36,10 +36,10 @@
                                 <thead
                                     class="text-xs justify-between font-semibold uppercase text-gray-500 bg-gray-100 rounded-lg overflow-hidden ">
                                     <tr class="text-center">
-                                        <th class="w-10 whitespace-nowrap ">
+                                        <th class="p-2 w-10 whitespace-nowrap ">
                                             <div class="font-semibold text-center">No</div>
                                         </th>
-                                        <th class="p-2 whitespace-nowrap">
+                                        <th class="p-2 w-48 whitespace-nowrap">
                                             <div class="font-semibold text-center">Kegiatan</div>
                                         </th>
                                         <th class="p-2 whitespace-nowrap">
@@ -54,7 +54,7 @@
                                         <th class="p-2 whitespace-nowrap">
                                             <div class="font-semibold text-center">Angka Kredit</div>
                                         </th>
-                                        <th class="p-2 whitespace-nowrap">
+                                        <th class="p-2 whitespace-nowrap w-10">
                                             <div class="font-semibold text-center">Aksi</div>
                                         </th>
                                     </tr>
@@ -63,7 +63,7 @@
                                     <?php $no=0; ?>
                                     @foreach ($kegiatan as $kegiatan )
                                     <?php $no++; ?>
-                                    <tr class="hover:bg-gray-100 dark:hover:bg-gray-00">
+                                    <tr class="hover:bg-gray-100 dark:hover:bg-gray-00 py-2 ">
                                         <td class="p-2 whitespace-nowrap">
                                             <div class="text-center text-sm">{{ $no }}</div>
                                         </td>
@@ -84,21 +84,26 @@
                                         <td class="p-2 whitespace-nowrap">
                                             <div class="text-sm text-center">{{ $kegiatan->angka_kredit }}</div>
                                         </td>
-                                        <td class="inline-flex whitespace-nowrap items-center ">
+                                        <td class="py-0.5 inline-flex  whitespace-nowrap pr-4 ">
+
+
                                             <button
-                                                class=" inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
+                                                class="inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-blue-500 to-blue-700 group-hover:from-blue-500  hover:text-white dark:text-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-800">
                                                 <span
-                                                    class=" py-1 transition-all ease-in duration-75 bg-white  rounded-md group-hover:bg-opacity-0">
+                                                    class=" py-1 px-2 transition-all ease-in duration-75 bg-white  rounded-md group-hover:bg-opacity-0">
                                                     Edit
                                                 </span>
                                             </button>
+
                                             <button
-                                                class="inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-500  group-hover:from-red-500 group-hover:to-pink-500 hover:text-gray-400 dark:text-white focus:ring-4 focus:outline-none focus:ring-red-500 ">
+                                                class=" inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-500 to-pink-500 group-hover:from-red-500  hover:text-white dark:text-gray-800 focus:ring-4 focus:outline-none focus:ring-red-200 dark:focus:ring-red-800">
                                                 <span
-                                                    class=" py-1 transition-all ease-in duration-75 bg-red-500  rounded-md group-hover:bg-opacity-0">
+                                                    class=" py-1 px-2 transition-all ease-in duration-75 bg-white  rounded-md group-hover:bg-opacity-0">
                                                     Hapus
                                                 </span>
                                             </button>
+
+
                                         </td>
                                     </tr>
                                     @endforeach
