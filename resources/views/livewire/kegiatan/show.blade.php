557216b0cc6938a3cdcfe3ea303c80063f33f81d
anchdr <chaidir.andrian@gmail.com>
2022-03-24 14:26:17 +0800

    1

--- resources/views/livewire/kegiatan/show.blade.php
+++ resources/views/livewire/kegiatan/show.blade.php
@@ -86,7 +86,7 @@
                                         </td>
                                         <td class=" inline-flex  whitespace-nowrap pr-4 pt-2  ">
                                             <button
-                                                class="inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-blue-500 to-blue-700 group-hover:from-blue-500  hover:text-white dark:text-gray-800 focus:ring-2 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-800">
+                                                class="inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-blue-500 to-blue-700 group-hover:from-blue-500  hover:text-white dark:text-gray-800 focus:ring-2 focus:outline-none focus:ring-blue-200">
                                                 <span
                                                     class=" py-1 px-2 transition-all ease-in duration-75 bg-white  rounded-md group-hover:bg-opacity-0">
                                                     Edit
@@ -94,7 +94,7 @@
                                             </button>
 
                                             <button
-                                                class=" inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-500 to-pink-500 group-hover:from-red-500  hover:text-white dark:text-gray-800 focus:ring-2 focus:outline-none focus:ring-red-200 dark:focus:ring-red-800">
+                                                class=" inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-500 to-pink-500 group-hover:from-red-500  hover:text-white dark:text-gray-800 focus:ring-2 focus:outline-none focus:ring-red-200">
                                                 <span
                                                     class=" py-1 px-2 transition-all ease-in duration-75 bg-white  rounded-md group-hover:bg-opacity-0">
                                                     Hapus
--- resources/views/livewire/operator/show.blade.php
+++ resources/views/livewire/operator/show.blade.php
@@ -90,19 +90,19 @@
                                                     </td>
                                                     <td class="inline-flex whitespace-nowrap place-items-center">
                                                         <button
-                                                            class="inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
+                                                        class="inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-blue-500 to-blue-700 group-hover:from-blue-500  hover:text-white dark:text-gray-800 focus:ring-2 focus:outline-none focus:ring-blue-200">
                                                             <span
-                                                                class=" py-1 transition-all ease-in duration-75 bg-white  rounded-md group-hover:bg-opacity-0">
+                                                                class=" py-1 px-2 transition-all ease-in duration-75 bg-white  rounded-md group-hover:bg-opacity-0">
                                                                 Edit
                                                             </span>
                                                         </button>
                                                         <button
-                                                            class="inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-500  group-hover:from-red-500 group-hover:to-pink-500 hover:text-gray-400 dark:text-white focus:ring-4 focus:outline-none focus:ring-red-500 ">
-                                                            <span
-                                                                class=" py-1 transition-all ease-in duration-75 bg-red-500  rounded-md group-hover:bg-opacity-0">
-                                                                Hapus
-                                                            </span>
-                                                        </button>
+                                                class=" inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-500 to-pink-500 group-hover:from-red-500  hover:text-white dark:text-gray-800 focus:ring-2 focus:outline-none focus:ring-red-200">
+                                                <span
+                                                    class=" py-1 px-2 transition-all ease-in duration-75 bg-white  rounded-md group-hover:bg-opacity-0">
+                                                    Hapus
+                                                </span>
+                                            </button>
                                                     </td>
                                                 </tr>
                                             @endforeach
